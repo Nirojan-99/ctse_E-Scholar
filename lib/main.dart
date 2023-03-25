89aae6f392f8b401cb88031154ea517c51eff15e
Abiramy-R <abhigrace2000@gmail.com>
2023-03-25 11:22:16 +0530

    manage imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:ctse_app/Screen/dashboard.dart';
 import 'package:ctse_app/Screen/degree_details.dart';
 import 'package:ctse_app/Screen/degree_screen.dart';
-
+import 'package:ctse_app/Screen/error_screen.dart';
 import 'package:ctse_app/Screen/faculty_details.dart';
 import 'package:ctse_app/Screen/faculty_screen.dart';
-
+import 'package:ctse_app/Screen/forgot_password.dart';
+import 'package:ctse_app/Screen/login_screen.dart';
+import 'package:ctse_app/Screen/module_details.dart';
+import 'package:ctse_app/Screen/module_screen.dart';
+import 'package:ctse_app/Screen/more_info.dart';
+import 'package:ctse_app/Screen/network_error.dart';
+import 'package:ctse_app/Screen/otp_screen.dart';
+import 'package:ctse_app/Screen/register_screen.dart';
+import 'package:ctse_app/Screen/reset_password.dart';
+import 'package:ctse_app/Screen/splash_screen.dart';
+import 'package:ctse_app/Screen/staff_details_screen.dart';
+import 'package:ctse_app/Screen/staff_screen.dart';
+import 'package:ctse_app/Screen/student_screen.dart';
+import 'package:ctse_app/Screen/students_details_screen.dart';
+import 'package:ctse_app/Theme/custom_dark_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
